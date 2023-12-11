4c3ccfd22cac96ce29fef49b5b638c42a4c9df1c
anshu1016 <69161719+anshu1016@users.noreply.github.com>
2023-12-11 11:11:43 +0000

    added top5 and bottom5 reviews function

--- controller/film.controller.js
+++ controller/film.controller.js
@@ -1,5 +1,5 @@
 const FilmModel = require("../model/film.model.js");
-const PersonModel  = require("../model/person.model.js");
+const PersonModel = require("../model/person.model.js");
 
 // Controller function to push film data
 const addFilm = async (req, res) => {
@@ -25,7 +25,6 @@ const addFilm = async (req, res) => {
 };
 
 //add rating and review
-
 
 const addRatingAndReview = async (req, res) => {
   const { movieId } = req.params;
@@ -73,8 +72,6 @@ const addRatingAndReview = async (req, res) => {
 
 // top 3 movie
 
-
-
 const getMovieReviewsWithUserDetails = async (req, res) => {
   const { movieId } = req.params;
 
@@ -113,14 +110,96 @@ const getMovieReviewsWithUserDetails = async (req, res) => {
   }
 };
 
+// Function to retrieve top 5 ratings and reviews of a movie
+const getTop5RatingsAndReviews = async (req, res) => {
+  const { movieId } = req.params;
 
+  try {
+    // Check if the movie exists
+    const movie = await FilmModel.findById(movieId);
+    if (!movie) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
 
+    // Sort reviews by rating in descending order
+    const sortedReviews = movie.reviews.sort((a, b) => b.rating - a.rating);
 
+    // Get the top 5 reviews with user details
+    const top5Reviews = sortedReviews.slice(0, 5).map(async (review) => {
+      const user = await PersonModel.findById(review.userId);
+      return {
+        rating: review.rating,
+        reviewText: review.review,
+        user: {
+          id: user._id,
+          username: user.username,
+          profilePicture: user.profilePicture,
+        },
+      };
+    });
 
+    // Wait for all promises to resolve
+    const resolvedReviews = await Promise.all(top5Reviews);
 
+    // Respond with the top 5 reviews and user details
+    res.status(200).json({
+      message: "Top 5 ratings and reviews fetched successfully",
+      reviews: resolvedReviews,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Function to retrieve bottom 5 ratings and reviews of a movie
+const getBottom5RatingsAndReviews = async (req, res) => {
+  const { movieId } = req.params;
+
+  try {
+    // Check if the movie exists
+    const movie = await FilmModel.findById(movieId);
+    if (!movie) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+
+    // Sort reviews by rating in ascending order
+    const sortedReviews = movie.reviews.sort((a, b) => a.rating - b.rating);
+
+    // Get the bottom 5 reviews with user details
+    const bottom5Reviews = sortedReviews.slice(0, 5).map(async (review) => {
+      const user = await PersonModel.findById(review.userId);
+      return {
+        rating: review.rating,
+        reviewText: review.review,
+        user: {
+          id: user._id,
+          username: user.username,
+          profilePicture: user.profilePicture,
+        },
+      };
+    });
+
+    // Wait for all promises to resolve
+    const resolvedReviews = await Promise.all(bottom5Reviews);
+
+    // Respond with the bottom 5 reviews and user details
+    res.status(200).json({
+      message: "Bottom 5 ratings and reviews fetched successfully",
+      reviews: resolvedReviews,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 
 
 module.exports = {
-  addFilm,addRatingAndReview,getMovieReviewsWithUserDetails
+  addFilm,
+  addRatingAndReview,
+  getMovieReviewsWithUserDetails,
+  getTop5RatingsAndReviews,
+  getBottom5RatingsAndReviews,
 };
--- router/film.router.js
+++ router/film.router.js
@@ -1,8 +1,17 @@
-const {addFilm,addRatingAndReview,getMovieReviewsWithUserDetails} = require("../controller/film.controller.js");
-const filmRouter = require("express").Router();  
-
+const {
+  
+  addFilm,
+  addRatingAndReview,
+  getMovieReviewsWithUserDetails,
+  getTop5RatingsAndReviews,
+  getBottom5RatingsAndReviews,
+} = require("../controller/film.controller.js");
+const filmRouter = require("express").Router();
 
 filmRouter.post("/addFilm", addFilm);
-filmRouter.post("/:movieId/rating",addRatingAndReview)
-filmRouter.get("/:movieId/reviews",getMovieReviewsWithUserDetails)
-module.exports = {filmRouter};
+filmRouter.post("/:movieId/rating", addRatingAndReview);
+filmRouter.get("/:movieId/reviews", getMovieReviewsWithUserDetails);
+
+filmRouter.get("/:movieId/top5",getTop5RatingsAndReviews);
+filmRouter.get("/:movieId/bottom5",getBottom5RatingsAndReviews);
+module.exports = { filmRouter };
